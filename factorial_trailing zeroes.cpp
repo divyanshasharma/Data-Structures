d1e834bf6a6e8b20fb4e96eb5d508115b98f54c6
Divyansha Sharma <51148118+divyanshasharma@users.noreply.github.com>
2021-06-02 13:13:12 +0530

    Add files via upload

---
+++ factorial_trailing zeroes.cpp
@@ -0,0 +1,14 @@
+//https://practice.geeksforgeeks.org/problems/trailing-zeroes-in-factorial5134/1
+class Solution
+{
+public:
+    int trailingZeroes(int N)
+    {
+        int c=0;
+        for(int i=5;N/i>=1;i*=5){
+            c+=N/i;
+        }
+        return c;
+    }
+};
+
